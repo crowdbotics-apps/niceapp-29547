b465c80c574801a4f180bedc470a6bd32e7b2661
Crowdbotics <team@crowdbotics.com>
2021-08-06 03:04:52 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "niceapp_29547Identifier",
+  url: "https://niceapp_29547Identifier.botics.co",
+  api: "https://niceapp_29547Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
